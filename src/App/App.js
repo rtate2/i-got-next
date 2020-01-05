4cc12cb7b40e411d99eca2219748b8939b9d2c80
Randy Tate <randytatejr@gmail.com>
2020-01-05 15:33:19 -0600

    completed react setup

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn-primary">BootStrap Button</button>
+    </div>
+  );
+}
+
+export default App;
