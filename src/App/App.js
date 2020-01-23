fd259cd47d1a562a186e9eb560ca91cb162db414
Randy Tate <randytatejr@gmail.com>
2020-01-22 22:49:24 -0600

    WIP

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+
+import {
+  BrowserRouter as Router, Route, Switch,
+} from 'react-router-dom';
+
+import Home from '../components/pages/Home/Home';
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <button className="btn-primary">BootStrap Button</button>
+      <Router>
+          <MyNavbar />
+          <Switch>
+            <Route path="/home" exact component={Home} />
+            {/* <Route path="/stuff/new" exact component={NewStuff} />
+            <Route path="/stuff" exact component={MyStuff} />
+            <Route path="/auth" exact component={Auth} />
+            <Route path="/stuff/12345/edit" exact component={EditButton} />
+            <Route path="/stuff/:itemId" exact component={SingleButton} /> */}
+          </Switch>
+        </Router>
     </div>
   );
 }
---
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  render() {
+    return (
+      <div className="MyNavbar">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/home">I Got Next</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/home">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="">Teams</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="">Waitlist</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="">Available Players</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
